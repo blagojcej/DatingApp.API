a86a08cadcbf8afd2e5c682b6bace1a46663b990
Blagojce Jovanoski <blagojcejovanoski@gmail.com>
2018-06-06 11:13:52 +0200

    Added Global Exception Handler.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -26,7 +26,10 @@ namespace DatingApp.API.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
         {
-            userForRegisterDto.UserName = userForRegisterDto.UserName.ToLower();
+            if (!string.IsNullOrEmpty(userForRegisterDto.UserName))
+            {
+                userForRegisterDto.UserName = userForRegisterDto.UserName.ToLower();
+            }
 
             if (await _repo.UserExists(userForRegisterDto.UserName))
             {
@@ -50,6 +53,8 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLoginDto)
         {
+            // throw new Exception("Computer says no!");
+
             var userFromRepo = await _repo.Login(userForLoginDto.UserName, userForLoginDto.Password);
 
             if (userFromRepo == null)
