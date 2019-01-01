64cacdc27944bde377b8b5ce09b472d27853f96f
blagojce_j <blagojcejovanoski@yahoo.com>
2019-01-01 20:05:45 +0100

    Changed tokenString to token.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -91,7 +91,7 @@ namespace DatingApp.API.Controllers
 
                 return Ok(new
                 {
-                    tokenString = GenerateJwtToken(appUser).Result,
+                    token = GenerateJwtToken(appUser).Result,
                     user = userToReturn
                 });
             }
